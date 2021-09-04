244c06d996a24f32df350ac442867578219a6ec6
Abimelec <69865179+AbdielOrtiz@users.noreply.github.com>
2021-09-04 03:49:40 +0000

    Mejora en el uso de event listener

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
-let x = document.getElementById("expUno");
-let y = x.childNodes;
-let longitud = (childList) => {
-    let p = 0;
-    while(childList[p] !== undefined){
-        p++;
+//funcion para mostrar y ocultar 
+const lista = document.getElementById("lista");
+lista.addEventListener("click", (e) => {
+    let element = e.target;
+    if (element && element.tagName === "H3") {
+        element.parentNode.classList.toggle("escondido");
+        element.parentNode.classList.toggle("mostrado");
     }
-    return p-1;
-}
-for(let i=1;i<longitud(y);i+=2){
-    if(y[i].innerHTML == "par"){
-        y[i].classList.add("par");
+})
+//primer experimento
+//mostrar cambiar estilo a elementos pares
+let expUno = document.getElementById("expUnoDiv").children;
+for (let i = 1; i < expUno.length; i++) {
+    if ((i - 1) % 2 === 0) {
+        expUno[i].classList.add("par");
+    }
+    else {
+        expUno[i].style.backgroundColor = "#fff"
     }
 }
-let containers = document.getElementsByClassName("container");
-for(let i=0;i<containers.length;i++){
-    containers[i].addEventListener("click",()=>{
-        containers[i].classList.toggle("escondido")
+//ver hoja de estilo para experimento 2
 
-    })
+//experimento 3
+//generar arreglo aleatoria
+function randomArray(length, max) {
+    let arreglo = [];
+    for (let i = 0; i < length; i++) {
+        arreglo[i] = Math.ceil(Math.random() * (max - 1) + 1);
+    }
+    return arreglo;
 }
-console.log(containers[0])
+//ordena arreglos usando el método burbuka
+function bubbleSort(array) {
+    for (let o = 0; o < array.length; o++) {
+        for (let p = 0; p < array.length - 1; p++) {
+            if (array[p] > array[p + 1]) {
+                [array[p], array[p + 1]] = [array[p + 1], array[p]]
+            }
+        }
+    }
+}
+//tomar el valor de input
+let valor = document.getElementById("valor").value;
+//seleccionar y asignar funcion a botón
+let arreglo = [];
+let orderButton = document.getElementById("orderButton");
+orderButton.addEventListener("click", x => { console.log(arreglo) })
+//orderButton.style = "display:none";
+let sendValue = document.getElementById("sendValue").addEventListener("click", () => {
+    console.log(valor);
+});

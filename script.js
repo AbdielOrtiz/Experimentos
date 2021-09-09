93efbca1a89a5a84aa4ff6133416132bf9cbb469
Abimelec <69865179+AbdielOrtiz@users.noreply.github.com>
2021-09-09 18:38:57 +0000

    Experimento 3 corregido

--- script.js
+++ script.js
@@ -7,25 +7,32 @@ lista.addEventListener("click", (e) => {
         element.parentNode.classList.toggle("mostrado");
     }
 })
-//primer experimento
+//-------------Experimento 1-------------
 //mostrar cambiar estilo a elementos pares
 let expUno = document.getElementById("expUnoDiv").children;
-for (let i = 1; i < expUno.length; i++) {
-    if ((i - 1) % 2 === 0) {
-        expUno[i].classList.add("par");
+let par = true;
+for (let element of expUno) {
+    if (par) {
+        element.style = "background: #F3F781;color: #000;"
+        par = false;
     }
     else {
-        expUno[i].style.backgroundColor = "#fff"
+        element.style = "background: #000;color: #F3F781;"
+        par = true
     }
 }
+
 //ver hoja de estilo para experimento 2
 
-//experimento 3
+//-------------Experimento 3-------------
 //generar arreglo aleatoria
+const numAleatorio = (max = 10) => {
+    return Math.ceil(Math.random() * (max - 1) + 1);
+}
 function randomArray(length, max) {
     let arreglo = [];
     for (let i = 0; i < length; i++) {
-        arreglo[i] = Math.ceil(Math.random() * (max - 1) + 1);
+        arreglo[i] = numAleatorio(max);
     }
     return arreglo;
 }
@@ -39,13 +46,43 @@ function bubbleSort(array) {
         }
     }
 }
-//tomar el valor de input
-let valor = document.getElementById("valor").value;
 //seleccionar y asignar funcion a botón
 let arreglo = [];
-let orderButton = document.getElementById("orderButton");
-orderButton.addEventListener("click", x => { console.log(arreglo) })
-//orderButton.style = "display:none";
+let miniContainer = document.querySelector(".miniContainer");
+let result = document.getElementById("result");
 let sendValue = document.getElementById("sendValue").addEventListener("click", () => {
-    console.log(valor);
+    let valor = document.getElementById("valor").value;
+    if (valor <= 100000) {
+        arreglo = randomArray(valor, 100);
+        result.innerHTML = "";
+        let r = document.createElement("li");
+        let msg = document.createTextNode("Desordenado:\n" + arreglo);
+        r.appendChild(msg);
+        result.append(r);
+    }
+    else{
+        alert("Para numeros tan grandes prueba el metodo\nQuicksort")
+    }
 });
+let orderButton = document.getElementById("orderButton");
+orderButton.addEventListener("click", x => {
+    bubbleSort(arreglo);
+    if (arreglo.length == 0) return null;
+    let r = document.createElement("li");
+    let msg = document.createTextNode("Ordenado:\n" + arreglo);
+    r.appendChild(msg);
+    result.append(r);
+})
+let clearInput = document.getElementById("clearInput");
+clearInput.addEventListener("click", () => {
+    if (arreglo.length == 0) return null;
+    arreglo = [];
+    let valor = document.getElementById("valor");
+    valor.value = "";
+    result.innerHTML = "";
+})
+//-------------Experimento 4-------------
+//algoritmo de ordenamiento Quicksort
+function quicksort(){
+    return 10;
+}
